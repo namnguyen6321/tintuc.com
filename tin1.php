bfe51d92ecd4b1c19a0f5dda504db4392642a35a
dinh nam <nguyendinhnama1k51dta@gmail.com>
2024-07-28 15:49:57 +0700

    thay doi

---
+++ tin1.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tin tức</title>
+    <link rel="stylesheet" href="tin1.css">
+</head>
+
+<body>
+    <div id="header">
+        <nav class="conteiner">
+            <a href="" id="logo">
+                <img src="assets/img/logo.jpg" alt="Tintuc">
+            </a>
+            <ul id="main-menu">
+                <li><a href="">Nóng </a></li>
+                <li><a href="">Mới</a></li>
+                <li><a href="">Video</a></li>
+                <li><a href="">Chủ Đề</a></li>
+                <li><a href="">Năng lượng tích cực</a></li>
+                <li><a href="">Khám phá thế giới </a></li>
+                <li><a href="">Khám phá Việt Nam</a></li>
+
+            </ul>
+        </nav>
+    </div>
+    <!-- <div>
+        <img src="assets/img/tin1-1.png" alt="banner">
+    </div> -->
+    <section id="banner">
+        <h1>Tin tức</h1>
+    </section>
+    <section class="noi-dung">
+        <ul class="danhsach-noidung clearfix">
+            <li>
+                <p class="service-image"><img src="assets/img/tin1.webp" alt="#" class="img-noidung"></p>
+                <div class="inner">
+                    <h3>Tổng Quan Hỗn Loạn Huyền Diệu</h3>
+                    <p class="info">tìm hiểu về Hỗn Loạn Huyền Diệu
+                        và những tộc hệ mạnh mẽ tại Thành Phố Của Những Kẻ Mộng Mơ
+                    </p>
+                </div>
+            </li>
+
+            <li>
+                <p class="service-image"><img src="assets/img/tin2.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>GIẢI ĐTCL OPEN TIẾP THEO SẼ ĐẾN MACAO!</h3>
+                    <p class="info">Sự kiện thi đấu nhánh mở trên quy mô toàn cầu sẽ diễn ra từ ngày 13/12 - 15/12.</p>
+                </div>
+            </li>
+
+            <li>
+                <p class="service-image"><img src="assets/img/tin3.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>LỜI MỜI ĐỘI TUYỂN ĐỐI TÁC GIẢI ĐẤU CHÂU Á THÁI BÌNH DƯƠNG 2025</h3>
+                    <p class="info">Đăng ký ngay và tham gia cùng chúng tôi trên con đường định hình cho tương lai
+                        Esports LMHT khu vực Châu Á Thái Bình Dương.</p>
+                </div>
+            </li>
+            <li>
+                <p class="service-image"><img src="assets/img/tin4.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>Hướng Dẫn Mua Vé CKTG 2024</h3>
+                    <p class="info">Tìm hiểu chi tiết mới nhất về cách thức mua vé CKTG!</p>
+                </div>
+            </li>
+
+            <li>
+                <p class="service-image"><img src="assets/img/tin5.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>TRANG PHỤC THẦN THOẠI & HÀNG HIỆU SẮP RA MẮT TRONG THÁNG 06/2024</h3>
+                    <p class="info">Danh sách các Trang Phục Hàng Hiệu và Thần Thoại sắp ra mắt</p>
+                </div>
+            </li>
+
+            <li>
+                <p class="service-image"><img src="assets/img/tin6.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>THÔNG TIN BẢN CẬP NHẬT 14.13</h3>
+                    <p class="info">Bản cập nhật 14.13 đã sẵn sàng "chào sân" 💦</p>
+                </div>
+            </li>
+            <li>
+                <p class="service-image"><img src="assets/img/tin7.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>ĐẤU TRƯỜNG CHÂN LÝ - THÔNG TIN BẢN CẬP NHẬT 14.13</h3>
+                    <p class="info">Đây là bản cập nhật dành cho Tactician’s Crown, vậy nên chúng tôi sẽ chỉ "phẩy tay
+                        chút nhẹ" với các điều chỉnh và Tiệc Pengu.</p>
+                </div>
+            </li>
+
+            <li>
+                <p class="service-image"><img src="assets/img/tin8.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>TỔNG QUAN TƯỚNG: AURORA</h3>
+                    <p class="info">Cô Phù Thủy Thỏ Kỳ Quái</p>
+                </div>
+            </li>
+
+            <li>
+                <p class="service-image"><img src="assets/img/tin9.webp" class="img-noidung" alt="#"></p>
+                <div class="inner">
+                    <h3>ESPORTS LMHT: XÂY DỰNG MỘT TƯƠNG LAI TƯƠI SÁNG</h3>
+                    <p class="info">Tìm hiểu thêm về những cập nhật thú vị về Esports LMHT trong năm 2025</p>
+                </div>
+            </li>
+        </ul>
+    </section>
+
+</body>
+
+</html>
